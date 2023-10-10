fc0159ae98be2e016c67aa59a633902d717a401b
Bouchra Akl <bushraakl1234@gmail.com>
2023-10-10 20:10:49 -0300

    first commit

---
+++ src/main/java/com/desafio/crudng/CrudNgApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.crudng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudNgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudNgApplication.class, args);
+	}
+
+}
